d1dff7a155a09d5bfd822d517ccf564a49055897
fahimavsharian <fahimavsharian@outlook.com>
2019-10-21 17:58:54 -0400

    First Commit for Blog

---
+++ src/build/precache-manifest.20572c939ca39dd24b19564bb03d7af3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a6530daeb461523af8fbcfdebc3059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd80dc209896d56fe228",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "cdeebca78dd666b19ee5",
+    "url": "/static/js/2.7f477852.chunk.js"
+  },
+  {
+    "revision": "dd80dc209896d56fe228",
+    "url": "/static/js/main.9ca0bc8b.chunk.js"
+  },
+  {
+    "revision": "d2495654a00f5af0bf8c",
+    "url": "/static/js/runtime-main.5f374983.js"
+  }
+]);
